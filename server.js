a7d5b3e89a0e56eb97e5b70d5449d16f1fda4f77
Glitch (yo-fcc-timestamp-microservice) <none>
2017-12-05 23:49:46 +0000

    Updated with Glitch

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.send("jack");
+  response.render("index");
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get("/:timestamp", function (request, response) {
+  var today = new Date(request.params.timestamp);
+  
+  if (!isNaN(request.params.timestamp)) {
+    response.send(
+      {
+        "unix" : Number(request.params.timestamp),
+        "natural" : new Date(request.params.timestamp*1000).toDateString()
+      }    
+    )
+  } else if (!isNaN(today)) {
+    response.send(
+      {
+        "unix" : today.getTime() / 1000,
+        "natural" : request.params.timestamp
+      }    
+    )
+  } else {
+    response.send(
+      {
+        "unix" : null,
+        "natural" : null
+      }    
+    )
+  }
+  
 });
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
